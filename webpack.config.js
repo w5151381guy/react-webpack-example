6e1e61e060f23c9899f638945e5f5c85f6d426d7
Andy Chen <andychen@koodata.com.tw>
2019-09-18 17:15:18 +0800

    add babel plugin

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react'],
+            plugins: ['syntax-dynamic-import', 'transform-class-properties'],
           },
         },
       },
